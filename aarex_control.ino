7a289fab48b753c62f8a1d6617f6c5155c433c9b
Johannes17S <33261265+Johannes17S@users.noreply.github.com>
2018-05-28 15:04:02 +0200

    Add files via upload

---
+++ aarex_control.ino
@@ -0,0 +1,52 @@
+/*
+ * control of arexx robot for human tracking
+ */
+#include <Wire.h>
+
+#define leftf 9
+#define rightf 5
+
+int32_t panError; 
+int speedLeft,speedRight;
+
+void setup() {
+  //Setup: Initalization
+
+  Wire.begin(2);
+  Serial.begin(115200);
+   
+  //pin 9 left foreward
+  pinMode(leftf, OUTPUT);
+  //pin 10 left backward
+  pinMode(10, OUTPUT);
+  //pin 5 right foreward
+  pinMode(rightf, OUTPUT);
+  //pin 6 right backward
+  pinMode(6, OUTPUT);
+
+  
+  //Then set the start value of the signals to zero: 
+  //digitalWrite(leftf, 0); //pin 9 to ground  
+  analogWrite(10, 0);   
+  analogWrite(rightf, 0);   
+  digitalWrite(6, 0); //pin 6 to ground
+  //Startup delay:
+  delay(2000);
+
+}
+
+void loop() {
+  panError = Wire.read();
+  Serial.println("panError:" + panError); 
+  map(panError, -159, 159, 0, 1);
+  Serial.println(" mapped panError:" + panError);
+
+  speedRight = panError*200; 
+  speedLeft = (1-panError)*200;
+
+  analogWrite(leftf, speedLeft);
+  analogWrite(rightf, speedRight);
+
+  delay(10);
+ 
+}
---
+++ tracking_robot.ino
@@ -0,0 +1,75 @@
+/*
+ * Sourcecode for tracking tags placed on humans or objects with a robot
+ * @author: Johannes Schäfer
+ * @institution: Hochschule Hamm-Lippstadt
+ */
+
+#include <SPI.h> 
+#include <Wire.h> 
+#include <Pixy.h>
+
+Pixy pixy;
+
+#define X_CENTER        ((PIXY_MAX_X-PIXY_MIN_X)/2)       
+
+uint16_t blocks;
+
+ static int i = 0;
+ int j;
+ char buf[32]; 
+ int32_t panError; //differnce from image center
+ int32_t Width, Height;
+ 
+void setup() {
+
+  
+  Serial.begin(9600);
+  Serial.print("Starting...\n");
+
+  Wire.begin();
+  
+  pixy.init();
+
+  if(!SPI.begin)Serial.print("No pixy cam found!");
+  else Serial.print("Initialisation successfull!");
+
+}
+
+void loop() 
+{
+ Wire.beginTransmission(2);
+ blockUpdate(); //first aquisition of blocks
+
+ if (blocks)
+  {
+   panError = X_CENTER-pixy.blocks[0].x;
+   Wire.write(panError);
+   
+    printBuf(); 
+  }
+ Wire.endTransmission();  
+}
+
+void blockUpdate()
+{
+ blocks = pixy.getBlocks();
+}
+
+void printBuf()
+{
+ // do this (print) every 50 frames because printing every
+    // frame would bog down the Arduino
+    if (i%50==0) 
+    {
+      sprintf(buf, "Detected %d:\n", blocks);
+      Serial.print(buf);
+      for (j=0; j<blocks; j++)
+      {
+        sprintf(buf, "  block %d: ", j);
+        Serial.print(buf); 
+        pixy.blocks[j].print();
+      }
+    } 
+}
+
+
