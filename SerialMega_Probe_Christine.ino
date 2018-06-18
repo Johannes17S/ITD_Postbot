6788d27530f91ded2d40112195a6e07aa56af615
ChristineAgethen <33144086+ChristineAgethen@users.noreply.github.com>
2018-06-18 13:51:51 +0200

    Add files via upload

---
+++ SerialMega_Probe_Christine.ino
@@ -0,0 +1,20 @@
+
+
+int zahl1=1;
+int zahl2;
+void setup() {
+  // put your setup code here, to run once:
+ Serial.begin(9600);
+ Serial1.begin(9600);
+ 
+}
+
+void loop() {
+ 
+    Serial1.println(zahl1);
+    delay(1000);
+
+    zahl2 = Serial1.read();
+    Serial.println(zahl2);
+
+}
