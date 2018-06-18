443739387a46846c88490cc0984fb1cc943328d5
ChristineAgethen <33144086+ChristineAgethen@users.noreply.github.com>
2018-06-18 13:51:18 +0200

    Add files via upload

---
+++ SerialArexx_Probe_Christine.ino
@@ -0,0 +1,16 @@
+int number1;
+int number2;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+     number1=Serial.read();
+     number2=number1 + 2;
+     delay(200);
+     Serial.print(number2);
+     delay(1000);
+    
+}
