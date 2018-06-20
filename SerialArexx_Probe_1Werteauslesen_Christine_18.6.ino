18239a3e2690b0423cf29142d8b92deddbdd6d5f
ChristineAgethen <33144086+ChristineAgethen@users.noreply.github.com>
2018-06-20 16:45:42 +0200

    Add files via upload

---
+++ SerialArexx_Probe_1Werteauslesen_Christine_18.6.ino
@@ -0,0 +1,79 @@
+#include<Wire.h>
+#include<SoftwareSerial.h>
+
+//define numbers to control arexx
+#define leftf 9 
+#define leftb 10 
+#define rightf 5 
+#define rightb 3
+
+int speedLeft;    //speed left
+int speedRight;   //speed right
+int readValue;    //value from Arduino Mega
+
+
+void setup() {
+  Serial.begin(9600);
+  
+  //pin 9 left foreward 
+   pinMode(leftf, OUTPUT); 
+  //pin 10 left backward 
+  pinMode(leftb, OUTPUT); 
+  //pin 5 right foreward 
+   pinMode(rightf, OUTPUT); 
+  //pin 3 right backward 
+  pinMode(rightb, OUTPUT);
+
+  //Then set the start value of the signals to zero:  
+  analogWrite(leftf, 0); //pin 9 to ground   
+  analogWrite(leftb, 0);  //pin 10 to ground  
+  analogWrite(rightf, 0); //pin 5 to ground  
+  analogWrite(rightb, 0); //pin 3 to ground
+}
+
+void loop() {
+
+     //get the value from the Arduino Mega
+     readValue=Serial.read();
+     Serial.print(readValue);
+     
+     
+      if(readValue==1){           //turn left
+         speedRight =1*0;  
+         speedLeft =1*175; 
+         analogWrite(leftf, speedLeft); 
+         analogWrite(rightf, speedRight);
+         analogWrite(rightb, 0); //pin 3 to ground
+         analogWrite(leftb, 0);  //pin 10 to ground          
+        }
+     else if(readValue==2){      //turn right
+         speedRight =1*175;  
+         speedLeft =1*0; 
+         analogWrite(leftf, speedLeft); 
+         analogWrite(rightf, speedRight);
+         analogWrite(rightb, 0); //pin 3 to ground
+         analogWrite(leftb, 0);  //pin 10 to ground      
+       }
+     else if(readValue==3){      //turn forward
+         speedRight =1*255;  
+         speedLeft =1*255; 
+         analogWrite(leftf, speedLeft); 
+         analogWrite(rightf, speedRight);
+         analogWrite(rightb, 0); //pin 3 to ground
+         analogWrite(leftb, 0);  //pin 10 to ground 
+       } 
+     else if(readValue==4){      //turn backward
+         speedRight =1*200;  
+         speedLeft =1*200; 
+         analogWrite(leftb, speedLeft); 
+         analogWrite(rightb, speedRight); 
+         analogWrite(leftf, 0);  //pin 9 to ground     
+         analogWrite(rightf, 0); //pin 5 to ground     
+       }
+     else if(readValue==5){      //brake
+         analogWrite(leftf, 0);  //pin 9 to ground     
+         analogWrite(rightf, 0); //pin 5 to ground  
+         analogWrite(rightb, 0); //pin 3 to ground
+         analogWrite(leftb, 0);  //pin 10 to ground      
+       }
+}
